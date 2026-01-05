cea353e44b19b11520ea4ff71bf965d0e41833bf
zeonaraa <rpsmaster12@gmail.com>
2026-01-05 13:40:37 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,34 @@
+const dataMusang = [
+  {
+    nama: "Tariq Qayyum",
+    facebook: "hhttps://www.facebook.com/profile.php?id=61579089680642",
+    jenis: "Rip $1 Gate",
+    bukti: [
+      "https://h.top4top.io/p_3657o3jc30.jpg"
+    ]
+  },
+  {
+    nama: "Welcome Cuan",
+    facebook: "https://www.facebook.com/ciro.alvez.948916",
+    jenis: "Rip 1$ Gate",
+    bukti: [
+      "https://a.top4top.io/p_3657viwej0.jpg"
+    ]
+  },
+  {
+    nama: "Andana Dika",
+    facebook: "https://www.facebook.com/mas.firman.79827",
+    jenis: "Rip 1$ Gate",
+    bukti: [
+      "https://h.top4top.io/p_3657ohsp31.jpg"
+    ]
+  },
+  {
+    nama: "Linda Lismawati",
+    facebook: "https://www.facebook.com/profile.php?id=61562396451982",
+    jenis: "Rip 1$ Gate",
+    bukti: [
+      "https://i.top4top.io/p_3657uljca0.jpg"
+    ]
+  },
+];
